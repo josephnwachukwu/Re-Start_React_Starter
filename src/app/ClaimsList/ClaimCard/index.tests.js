c0dffe9ea384f982d5ff3d56545a0e54ee576a9d
Charles Fu <cfu@tandemseven.com>
2016-12-05 14:43:50 -0500

    updated SVG test to include both pinned and unpinned icons

--- src/app/ClaimsList/ClaimCard/index.tests.js
+++ src/app/ClaimsList/ClaimCard/index.tests.js
@@ -3,12 +3,13 @@ import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import ClaimCard from './index.js'
 import Pin from '../../../theme/icons/Pin-1.svg'
+import Unpin from '../../../theme/icons/Unpin-1.svg'
 
 function onClick () {
   alert('ClaimCard Clicked')
 }
 
-const claim = {
+const claimPinned = {
   IsPinned: true,
   ClaimNumber: 'WC10003',
   PatientFirstName: 'Don',
@@ -17,23 +18,33 @@ const claim = {
   DOI: '2016-04-19T00:00:00.000Z'
 }
 
+const claimUnpinned = {
+  IsPinned: false,
+  ClaimNumber: 'WC10003',
+  PatientFirstName: 'Don',
+  PatientLastName: 'Draper',
+  DOB: '1973-02-22T00:00:00.000Z',
+  DOI: '2016-04-19T00:00:00.000Z'
+}
+
 describe('Claim Card component', function () {
-  it('has an icon', function () {
-    expect(shallow(<ClaimCard claim={claim} onClick={onClick} />).find(Pin).length).to.equal(1)
+  it('has the correct icons', function () {
+    expect(shallow(<ClaimCard claim={claimPinned} onClick={onClick} />).find(Pin).length).to.equal(1)
+    expect(shallow(<ClaimCard claim={claimUnpinned} onClick={onClick} />).find(Unpin).length).to.equal(1)
   })
 
   it('has the correct DOB and DOI', function () {
-    const claimCard = shallow(<ClaimCard claim={claim} onClick={onClick} />)
+    const claimCard = shallow(<ClaimCard claim={claimPinned} onClick={onClick} />)
 
     expect(claimCard.state().DOB).to.equal('02/21/1973')
     expect(claimCard.state().DOI).to.equal('04/18/2016')
   })
 
   it('has the correct name', function () {
-    expect(shallow(<ClaimCard claim={claim} onClick={onClick} />).state().name).to.equal('Draper, Don')
+    expect(shallow(<ClaimCard claim={claimPinned} onClick={onClick} />).state().name).to.equal('Draper, Don')
   })
 
   it('has an onClick function', function () {
-    expect(shallow(<ClaimCard claim={claim} onClick={onClick} />).props().onClick).to.equal(onClick)
+    expect(shallow(<ClaimCard claim={claimPinned} onClick={onClick} />).props().onClick).to.equal(onClick)
   })
 })
