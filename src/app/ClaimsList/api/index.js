61b5181167a866444b532b7deb93e0a2acfd3918
Diego Concha <dconcha@tandemseven.com>
2016-12-07 17:58:35 -0500

    remove http prefix

--- src/app/ClaimsList/api/index.js
+++ src/app/ClaimsList/api/index.js
@@ -1,5 +1,5 @@
 function getClaimsList (adjusterId) {
-  const baseUrl = 'http://app-d-polarisgatewayapi.azurewebsites.net/api/v0.1/Gateway/claimsbyadjuster'
+  const baseUrl = '//app-d-polarisgatewayapi.azurewebsites.net/api/v0.1/Gateway/claimsbyadjuster'
   const url = `${baseUrl}/${adjusterId}/all`
 
   return fetch(url, {
