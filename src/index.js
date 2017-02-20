61f7f0b4365160c5cdd8b156c098963c5838b407
Richards Marks <rmarks@tandemseven.com>
2017-02-20 16:26:33 +0000

    Merged PR 80: setup google maps api injection into application boot
    In order to use the google maps api, we have to inject a `script` tag into the document.
    Since we don't know the api key and may need to get it from a web service data endpoint, we are unable to simply add the tag to the index.html file and must resort to a programatic method of getting the script loaded.
    
    I have built up a utility to get the api key - temporarily using my own api key for dev purposes.
    I have added a util to load the google maps api and finally kick off our react application once the api is loaded.

--- src/index.js
+++ src/index.js
@@ -14,21 +14,46 @@ import PatientInfo from './app/PatientInfo'
 import NotFound from './app/NotFound'
 import Sandbox from './app/Sandbox'
 
-ReactDOM.render(
-  <Theme>
-    <Router history={hashHistory}>
-      <Route path='/' component={MainLayout}>
-        <IndexRoute component={Dashboard} />
-        <Route path='activeclaims' component={ClaimsList} />
-        <Route path='dashboard' component={Dashboard} />
-        <Route path='patientinfo' component={PatientInfo} />
-        <Route path='sandbox' component={Sandbox} />
-        <Route path='createorder' component={CreateOrder}>
-          <Route path='product' component={ProductPage} />
+import loadScript from './app/Shared/Utils/loadScript'
+import { googleMapsApi } from './app/Shared/Utils/googleMapsApi'
+
+function launchApp () {
+  ReactDOM.render(
+    <Theme>
+      <Router history={hashHistory}>
+        <Route path='/' component={MainLayout}>
+          <IndexRoute component={Dashboard} />
+          <Route path='activeclaims' component={ClaimsList} />
+          <Route path='dashboard' component={Dashboard} />
+          <Route path='patientinfo' component={PatientInfo} />
+          <Route path='sandbox' component={Sandbox} />
+          <Route path='createorder' component={CreateOrder}>
+            <Route path='product' component={ProductPage} />
+          </Route>
         </Route>
-      </Route>
-      <Route path='*' component={NotFound} />
-    </Router>
-  </Theme>,
-  document.querySelector('#app-root')
-)
+        <Route path='*' component={NotFound} />
+      </Router>
+    </Theme>,
+    document.querySelector('#app-root')
+  )
+}
+
+function boot () {
+  googleMapsApi.getApiKey().then(payload => {
+    const { apiKey } = payload
+
+    window.bootStage2 = () => {
+      window.bootStage2 = null
+      launchApp()
+    }
+
+    const url = googleMapsApi.getUrl({
+      apiKey,
+      libraries: ['places']
+    }, 'bootStage2')
+
+    loadScript(url)
+  })
+}
+
+boot()
