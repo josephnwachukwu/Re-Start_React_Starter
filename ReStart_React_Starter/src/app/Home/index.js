18e68410e0a080d786bc98c30e7967c98312e29f
Joseph Nwachukwu <josephnwachukwu@gmail.com>
2017-03-05 03:04:54 -0500

    Add files via upload

---
+++ ReStart_React_Starter/src/app/Home/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './index.css'
+
+export default class Home extends Component {
+
+  render () {
+    return (
+      <div className='grid'>
+        <div className='grid__col-auto hero'>
+          <h1>Re-Start</h1>
+        A Mobile first responsive library of layouts and components
+        </div>
+      </div>
+    )
+  }
+}
